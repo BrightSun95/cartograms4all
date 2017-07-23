68c29994e148e57dd796d33b5d23790a3071f902
Luke Tanner <latanner.ucsc.edu>
2017-07-23 04:03:04 -0700

    savingByFile

--- app/js/session.js
+++ app/js/session.js
@@ -43,3 +43,70 @@ function generateSessionID(length) {
   }
   return result;
 }
+
+//Save CSV to uploader/upload path via an ajax call
+//The saved CSV can be use for other user as it is public
+function saveByFile(userCSV) {
+  console.log("saveByFile()");
+  var data = new FormData();
+  data.append("input_csv", userCSV);
+  data.append("name", userCSV.name)
+  data.append("session_id", userSessionID);
+
+  $.ajax({
+    url: 'uploader/upload-manager.php',
+    type: 'POST',
+    data: data,
+    cache: false,
+    processData: false, // Don't process the files
+    contentType: false, // jQuery will tell the server its a query string request
+    success: function(data, textStatus, jqXHR) {
+      if (typeof data.error === 'undefined') {
+        // Success so call function to process the form
+        //submitForm(event, data);
+  console.log('Success' + textStatus);
+      } else {
+        // Handle errors here
+        console.log('ERRORS: ' + data.error);
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log('ERRORS: ' + textStatus);
+    }
+  });
+*/
+}
+
+//Save CSV to uploader/upload path via an ajax call
+//The saved CSV can be use for other user as it is public
+function saveByName() {
+  console.log("saveByName()");
+/*
+  var data = new FormData();
+  data.append("input_csv", userCSV);
+  data.append("name", userCSV.name)
+  data.append("session_id", userSessionID);
+
+  $.ajax({
+    url: 'uploader/upload-manager.php',
+    type: 'POST',
+    data: data,
+    cache: false,
+    processData: false, // Don't process the files
+    contentType: false, // jQuery will tell the server its a query string request
+    success: function(data, textStatus, jqXHR) {
+      if (typeof data.error === 'undefined') {
+        // Success so call function to process the form
+        //submitForm(event, data);
+  console.log('Success' + textStatus);
+      } else {
+        // Handle errors here
+        console.log('ERRORS: ' + data.error);
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log('ERRORS: ' + textStatus);
+    }
+  });
+*/
+}
